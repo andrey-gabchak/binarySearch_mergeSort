32f0db43aa242d5080002b5ae42e38ca158736b7
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-22 03:30:28 +0300

    Test for array which contain one element. Renamed 'result' to 'actualIndex'.

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -16,44 +16,53 @@ class BinarySearchTest {
     void testBinarySearchForArrayWithShiftedIndex() {
         int[] inputArray = new int[]{5, 5, 1, 3, 4, 5, 5, 5, 5};
         int soughtValue = 5;
-        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
         int expectedIndex = 0;
-        assertEquals(expectedIndex, result);
+        assertEquals(expectedIndex, actualIndex);
     }
 
     @Test
     void testBinarySearchDefault() {
         int[] inputArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int soughtValue = 5;
-        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
         int expectedIndex = 4;
-        assertEquals(expectedIndex, result);
+        assertEquals(expectedIndex, actualIndex);
     }
 
     @Test
     void testBinarySearchForSameNumbers() {
         int[] inputArray = new int[]{5, 5, 7, 7, 7, 10, 10, 12, 12};
         int soughtValue = 5;
-        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
         int expectedIndex = 0;
-        assertEquals(expectedIndex, result);
+        assertEquals(expectedIndex, actualIndex);
     }
 
     @Test
     void testBinarySearchForSameNumbersRightHalfOfArray() {
         int[] inputArray = new int[]{5, 5, 7, 7, 7, 10, 10, 11, 12, 12};
         int soughtValue = 12;
-        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
         int expectedIndex = 7;
-        assertEquals(expectedIndex, result);
+        assertEquals(expectedIndex, actualIndex);
     }
 
     @Test
     void testBinarySearchForNonexistentKey() {
         int[] inputArray = new int[]{5, 5, 7, 7, 7, 10, 10, 11, 12, 12};
         int soughtValue = 13;
-        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
         int expectedIndex = -1;
-        assertEquals(expectedIndex, result);
+        assertEquals(expectedIndex, actualIndex);
+    }
+
+    @Test
+    void testBinarySearchForOneElement() {
+        int[] inputArray = new int[]{1};
+        int soughtValue = 1;
+        int actualIndex = binarySearch.binarySearch(inputArray, soughtValue);
+        int expectedIndex = 0;
+        assertEquals(expectedIndex, actualIndex);
     }
 }
