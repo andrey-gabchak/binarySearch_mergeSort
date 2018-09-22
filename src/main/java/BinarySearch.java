af024615d92d5a75e3fe759630c427ed5654a1c7
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-22 03:10:23 +0300

    Initial commit

---
+++ src/main/java/BinarySearch.java
@@ -0,0 +1,23 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+
+    int binarySearch(int[] array, int key) {
+        int leftIndex = 0;
+        int rightIndex = array.length - 1;
+
+        while (leftIndex <= rightIndex) {
+            int midIndex = (leftIndex + rightIndex) >>> 1;
+            int midValue = array[midIndex];
+            if (key > midValue) {
+                leftIndex = midIndex + 1;
+            } else if (key < midValue) {
+                rightIndex = midIndex - 1;
+            } else {
+                return midIndex;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class BinarySearchTest {
+
+    private static BinarySearch binarySearch;
+
+    @BeforeAll
+    static void setUp() {
+        binarySearch = new BinarySearch();
+    }
+
+    @org.junit.jupiter.api.Test
+    void testBinarySearchForArrayWithShiftedIndex() {
+        int[] inputArray = new int[]{5, 5, 1, 3, 4, 5, 5, 5, 5};
+        int soughtValue = 5;
+        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int expectedIndex = 0;
+        assertEquals(expectedIndex, result);
+    }
+
+    @Test
+    void testBinarySearchDefault() {
+        int[] inputArray = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int soughtValue = 5;
+        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int expectedIndex = 4;
+        assertEquals(expectedIndex, result);
+    }
+
+    @Test
+    void testBinarySearchForSameNumbers() {
+        int[] inputArray = new int[]{5, 5, 7, 7, 7, 10, 10, 12, 12};
+        int soughtValue = 5;
+        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int expectedIndex = 0;
+        assertEquals(expectedIndex, result);
+    }
+
+    @Test
+    void testBinarySearchForSameNumbersRightHalfOfArray() {
+        int[] inputArray = new int[]{5, 5, 7, 7, 7, 10, 10, 11, 12, 12};
+        int soughtValue = 12;
+        int result = binarySearch.binarySearch(inputArray, soughtValue);
+        int expectedIndex = 7;
+        assertEquals(expectedIndex, result);
+    }
+
+
+}
