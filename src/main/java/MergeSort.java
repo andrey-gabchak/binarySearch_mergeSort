c72f0aeaa44067b64e1cf1f267d6c786eab51a97
andrey-gabchak <andrey.gabchak@gmail.com>
2018-09-23 05:15:24 +0300

    Merge sort using buffer array

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -1,9 +1,44 @@
+import java.util.Arrays;
+
 public class MergeSort {
 
-    int[] mergeSort(int[] unSortedArray) {
+    void mergeSort(int[] inputArray, int leftIndex, int rightIndex) {
 
-        return unSortedArray;
+        if (rightIndex <= leftIndex) {
+            return;
+        }
+
+        if (leftIndex + 1 < rightIndex) {
+
+        }
+        int mid = leftIndex + (rightIndex - leftIndex) / 2;
+        mergeSort(inputArray, leftIndex, mid);
+        mergeSort(inputArray, mid + 1, rightIndex);
+
+        int[] bufferArray = Arrays.copyOf(inputArray, inputArray.length);
+
+        for (int k = leftIndex; k <= rightIndex; k++) {
+            bufferArray[k] = inputArray[k];
+        }
+
+        int firstIndex = leftIndex, lastIndex = mid + 1;
+
+        for (int i = leftIndex; i <= rightIndex; i++) {
+
+            if (firstIndex > mid) {
+                inputArray[i] = bufferArray[lastIndex];
+                lastIndex++;
+            } else if (lastIndex > rightIndex) {
+                inputArray[i] = bufferArray[firstIndex];
+                firstIndex++;
+            } else if (bufferArray[lastIndex] < bufferArray[firstIndex]) {
+                inputArray[i] = bufferArray[lastIndex];
+                lastIndex++;
+            } else {
+                inputArray[i] = bufferArray[firstIndex];
+                firstIndex++;
+            }
+        }
     }
-
 
 }
--- src/test/java/MergeSortTest.java
+++ src/test/java/MergeSortTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,16 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class MergeSortTest {
 
     private static MergeSort mergeClass;
+
     @BeforeAll
     static void setUp() {
         mergeClass = new MergeSort();
     }
 
     @Test
-    void mergeSort() {
-        int[] inputArray = new int[] {2100, 23, 40, 24, 2, 1, 10, 18, 11};
-        int[] expectedArray = new int[]{1, 2, 10, 11, 18, 23, 24, 40, 2100};
-        int[] actualArray = mergeClass.mergeSort(inputArray);
-        assertArrayEquals(expectedArray, actualArray);
+    void testMergeSort() {
+        int[] inputArray = new int[]{2100, 23, 40, 24, 2, 1, 10, 18, 11, 25, 13, 17, 9, 3};
+        int[] expectedArray = new int[]{1, 2, 3, 9, 10, 11, 13, 17, 18, 23, 24, 25, 40, 2100};
+        mergeClass.mergeSort(inputArray, 0, inputArray.length - 1);
+        assertArrayEquals(expectedArray, inputArray);
     }
 }
